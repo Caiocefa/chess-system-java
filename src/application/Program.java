de2bbda4602840175b506c18fe440aad71372eda
Caio Cesar <caiocesarfandrade@gmail.com>
2022-01-27 20:41:37 -0300

    Creating Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
 		
-		System.out.println("Jogo de xadrez");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 
 	}
 
